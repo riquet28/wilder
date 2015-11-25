1ef6b0bba2b8c174d9029881fb30577193a2a761
riquet <eric.merlin3@gmail.com>
2015-11-25 01:51:47 +0100

    Enrichissement de la cartouche profil, du profil user et ajout méthode image selon langage

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,11 +1,23 @@
 module UserHelper
 
-	def connecte(current_user)
+	# Fonctionne pas
+  def connecte(current_user)
 		if user_signed_in?
 			"Connecté"
 		else
 			"Hors-ligne"
 		end
 	end
+
+  # Affichage du logo selon langage
+  def logo(langage)
+    if langage == "Ruby"
+      image_tag("rails.png")
+    elsif langage == "Php"
+      image_tag("php.png")
+    else
+      image_tag("javascript.png")
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_attached_file :avatar, styles: {
-    medium: "300x300>",
+    medium: "200x200>",
     thumb: "100x100>" 
   }, 
     default_url: "/images/:style/missing.png"
