91749f0ff450553757df2bbef5d31e1a4f9bc08f
riquet <eric.merlin3@gmail.com>
2015-11-26 03:19:21 +0100

    Avancement dans cartouche liste-membre

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -79,6 +79,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:user_id, :content)
+      params.require(:post).permit(:user_id, :content, :pseudo, :avatar)
     end
 end
