ef5de6b42b0adefaccbbc4f4ec1d3a0fd5cf28f1
riquet <eric.merlin3@gmail.com>
2015-12-30 04:22:17 +0100

    Ajout du nb de challenges dans la cartouche, mise en place anti-triche avec uniqueness

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -3,7 +3,7 @@ class Participation < ActiveRecord::Base
   belongs_to :challenge
   belongs_to :user
 
-  validates :challenge_id, presence: true
+  validates :challenge_id, presence: true, uniqueness: { scope: :user_id }
   validates :user_id, presence: true
 
   after_create :give_points_to_user
