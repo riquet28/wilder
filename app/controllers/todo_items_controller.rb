7deda59d61bb4207261c0a2a5bce24533f99ee9d
riquet <eric.merlin3@gmail.com>
2016-01-03 03:05:06 +0100

    Css ok

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,6 +2,7 @@ class TodoItemsController < ApplicationController
   before_action :set_todo_list
   before_action :set_todo_item, except: [:create]
 
+
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
