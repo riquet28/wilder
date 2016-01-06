cb8f8766cd15871ee42ff4cf9c2487b268d2055c
riquet <eric.merlin3@gmail.com>
2016-01-06 18:51:54 +0100

    commit debug

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,11 +5,7 @@ class UserController < ApplicationController
 	def index
     @titre = "La Promo"
     @users = User.all
-    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-      marker.lat user.latitude
-      marker.lng user.longitude
-      marker.infowindow user.pseudo
-    end
+    
 	end
 
   def show
