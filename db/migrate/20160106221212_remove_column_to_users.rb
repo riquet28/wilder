1a2fe6f51f35201bd63bf492f9498bf06d0506d6
riquet <eric.merlin3@gmail.com>
2016-01-11 10:49:09 +0100

    Suppression migration prob heroku

--- db/migrate/20160106221212_remove_column_to_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnToUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :avatar_file_name, :string
-    remove_column :users, :avatar_content_type, :string
-    remove_column :users, :avatar_file_size, :integer
-    remove_column :users, :avatar_updated_at, :datetime
-  end
-end
