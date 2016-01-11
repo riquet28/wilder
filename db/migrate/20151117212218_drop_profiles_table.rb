7f90f9c5b494e16e39e7f200004ff5f2842b530d
riquet <eric.merlin3@gmail.com>
2016-01-11 10:40:37 +0100

    Suppression de migration, prob deploiement heroku

--- db/migrate/20151117212218_drop_profiles_table.rb
+++
@@ -1,4 +0,0 @@
-class DropProfilesTable < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20151124222218_drop_profil_table.rb
+++
@@ -1,5 +0,0 @@
-class DropProfilTable < ActiveRecord::Migration
-  def change
-     drop_table :profiles
-  end
-end
