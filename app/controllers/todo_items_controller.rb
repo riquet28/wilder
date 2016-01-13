b662ad94038b181f0b854f50185acc9b778fed38
riquet <eric.merlin3@gmail.com>
2016-01-13 17:57:20 +0100

    Ajax sur les taches ok

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -5,17 +5,22 @@ class TodoItemsController < ApplicationController
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
-    redirect_to @todo_list
+    respond_to do |format|
+      if @todo_item.save
+        format.html { redirect_to @todo_list, notice: 'Votre tache a bien été créée !' }
+        format.js
+      end
+    end
   end
 
   def destroy
     @todo_item = @todo_list.todo_items.find(params[:id])
-    if @todo_item.destroy
+    @todo_item.destroy
+    respond_to do |format|
       flash[:success] = "Votre tache a été supprimée !"
-    else
-      flash[:error] = "Votre tache ne peut pas être supprimée !"
+      format.html { redirect_to @todo_list }
+      format.js
     end
-    redirect_to @todo_list
   end
 
   def complete
